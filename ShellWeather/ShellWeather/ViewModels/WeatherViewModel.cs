a1badc1062fdf85e03ef7b26fa319f96d4be30d3
Justin Horner <mail@justinhorner.me>
2020-04-13 20:09:49 -0700

    package upgrades, WIP
    upgraded xamarin packages
    added darkSky api
    wip on WeatherPage

---
+++ ShellWeather/ShellWeather/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShellWeather.ViewModels
+{
+    public class WeatherViewModel : BaseViewModel
+    {
+        private string apiKey = "";
+        private DarkSky.Services.DarkSkyService darkSkyService;
+        public Command GetForecastCommand { get; set; }
+
+        public WeatherViewModel()
+        {
+            Title = "Weather";
+            darkSkyService = new DarkSky.Services.DarkSkyService(apiKey);
+            GetForecastCommand = new Command(async () => await ExecuteGetForecastCommandAsync());
+        }
+
+        private async Task ExecuteGetForecastCommandAsync()
+        {
+            var coords = (0.0, 0.0);
+            var forecast = await darkSkyService.GetForecast(coords.Item1, coords.Item2);
+        }
+    }
+}
---
+++ ShellWeather/ShellWeather/Views/WeatherPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using ShellWeather.ViewModels;
+using Xamarin.Forms;
+
+namespace ShellWeather.Views
+{
+    public partial class WeatherPage : ContentPage
+    {
+        WeatherViewModel viewModel;
+
+        public WeatherPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new WeatherViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            viewModel.GetForecastCommand.Execute(null);
+        }
+    }
+}
