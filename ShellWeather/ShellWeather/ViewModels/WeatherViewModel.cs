2c4f46f8bcd38e2dd164e0ac480f34cbb37257b6
Justin Horner <justin.horner@offerpad.com>
2020-04-13 21:21:30 -0700

    more wip, current temp loading

--- ShellWeather/ShellWeather/ViewModels/WeatherViewModel.cs
+++ ShellWeather/ShellWeather/ViewModels/WeatherViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using DarkSky.Models;
 using Xamarin.Forms;
 
 namespace ShellWeather.ViewModels
@@ -9,6 +10,41 @@ namespace ShellWeather.ViewModels
         private string apiKey = "";
         private DarkSky.Services.DarkSkyService darkSkyService;
         public Command GetForecastCommand { get; set; }
+        
+        public Forecast Forecast { get; set; }
+
+        private string _currentTemp { get; set; }
+        public string CurrentTemp
+        {
+            get => _currentTemp;
+            set
+            {
+                _currentTemp = value;
+                OnPropertyChanged(nameof(CurrentTemp));
+            }
+        }
+
+        private string _lastUpdated { get; set; }
+        public string LastUpdated
+        {
+            get => _lastUpdated;
+            set
+            {
+                _lastUpdated = value;
+                OnPropertyChanged(nameof(LastUpdated));
+            }
+        }
+        
+        private string _summary { get; set; }
+        public string Summary
+        {
+            get => _summary;
+            set
+            {
+                _summary = value;
+                OnPropertyChanged(nameof(Summary));
+            }
+        }
 
         public WeatherViewModel()
         {
@@ -20,7 +56,17 @@ namespace ShellWeather.ViewModels
         private async Task ExecuteGetForecastCommandAsync()
         {
             var coords = (0.0, 0.0);
-            var forecast = await darkSkyService.GetForecast(coords.Item1, coords.Item2);
+            var result = await darkSkyService.GetForecast(coords.Item1, coords.Item2);
+
+            if (result.IsSuccessStatus)
+            {
+                Forecast = result.Response;
+                var fahrenheitTemp = Math.Round(Forecast?.Currently?.Temperature ?? 0);
+                var celsiusTemp = Math.Round((fahrenheitTemp - 32) / 1.8);
+                Summary = Forecast?.Minutely?.Summary ?? "Unknown";
+                CurrentTemp = $"{fahrenheitTemp} ℉ ({celsiusTemp} ℃)";
+                LastUpdated = $"Last updated: {Forecast?.Currently?.DateTime.ToString("G") ?? string.Empty}";
+            }
         }
     }
 }
